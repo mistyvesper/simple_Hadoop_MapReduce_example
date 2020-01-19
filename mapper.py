204d74f378a3afaffb674c8804bf63d1287a5616
mistyvesper <mistyvesper@gmail.com>
2020-01-19 08:54:36 +0000

    Updating mapper.py to change all letter to lowercase, remove stopwords, and remove punctuation

--- mapper.py
+++ mapper.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import sys
+import re
+
+stopwords = set(['the', 'and'])
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    # convert to lowercase
+    # remove punctuation using regular expressions
+    line = re.sub(r'[^\w\s]', '', line.strip().lower())
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwords:
+        	print '%s\t%s' % (word, "1")
